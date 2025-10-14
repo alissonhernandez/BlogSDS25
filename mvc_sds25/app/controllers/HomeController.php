2425472f26aa87ed3db82e1f3e8bd5dcf4bf556b
alissonhernandez <ha21011@ues.edu.sv>
2025-10-14 13:13:28 -0600

    diseño y estructura de home

--- mvc_sds25/app/controllers/HomeController.php
+++ mvc_sds25/app/controllers/HomeController.php
@@ -3,7 +3,12 @@ namespace app\controllers;
 
 class HomeController {
     public function index() {
-        $data = ["title" => "Pagina Principal", "descripcion" => "Este blog contiene las actividades realizadas durante la sds25"];
+        $data = ["title" => "SDS25 - Inicio", 
+        "proyecto" => "Bienvenido al Blog SDS25",
+        "descripcion" => "Este proyecto consiste en realizar un blog sobre la SDS25 utilizando el patrón MVC con PHP.",
+        "SDS25" => "¿Qué es la SDS25?",
+        "concepto" => "La Semana de Sistemas es un evento académico que se lleva a cabo en la UES FMO, con el objetivo de fomentar el aprendizaje y la práctica de habilidades relacionadas con nuestra carrera. Durante esta semana, los estudiantes tienen la oportunidad de participar en talleres, conferencias, hackatones y otras actividades que les permiten adquirir conocimientos prácticos y teóricos.",
+        "realizado" => "Realizado por: Alisson Andrea Hernández Arias"];
         return $this->view("home", $data);
     }
 
--- mvc_sds25/app/views/home.php
+++ mvc_sds25/app/views/home.php
@@ -1,5 +1,30 @@
-<h1><?= $descripcion?></h1>
-<p><?= $title ?></p>
-    <nav>
-        <a href="/mvc_sds25/public/">Home</a> |
-        <a href="/mvc_sds25/public/dia1">Dia 1</a> |
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/mvc_sds25/public/css/home.css">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="left-side"></div>
+
+        <div class="right-side">
+            <nav>
+                <a href="/mvc_sds25/public/">Home</a>
+                <a href="/mvc_sds25/public/dia1">Dia 1</a>
+                <a href="/mvc_sds25/public/dia2">Dia 2</a>
+            </nav>
+
+            <h1><?= $proyecto ?></h1>
+            <p><?= $descripcion ?></p>
+
+            <h1><?= $SDS25 ?></h1>
+            <p><?= $concepto ?></p>
+
+            <p><?= $realizado ?></p>
+        </div>
+    </div>
+</body>
+</html>
